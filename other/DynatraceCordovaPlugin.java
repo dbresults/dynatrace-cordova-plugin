c48b498b17fd9a0ad50e79d12c9a30d6bb534b1e
Jonathan Lui <jonathan.lui@dbresults.com.au>
2023-02-20 05:57:04 +0000

    Updated @dynatrace/cordova-plugin from NPM

--- other/DynatraceCordovaPlugin.java
+++ other/DynatraceCordovaPlugin.java
@@ -49,13 +49,14 @@ public class DynatraceCordovaPlugin extends CordovaPlugin {
         callbackContext.success("Privacy settings updated!");
 
         return true;
-      } else if (action.equals(ACTION_UEM_IDENTIFY_USER)) {
+      }
+      else if (action.equals(ACTION_UEM_IDENTIFY_USER)) {
         String userId = args.getJSONObject(0).getString("userId");
-  
+
         Dynatrace.identifyUser(userId);
         callbackContext.success("UserId: " + userId);
         return true;
-      } 
+      }
     } catch(Exception e) {
       System.err.println("Exception: " + e.getMessage());
       callbackContext.error(e.getMessage());
